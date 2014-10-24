c5d1b3cd1636c9de8d6198a81c4f9e10f438f6fb
Kevin Casey <kacasey@berkeley.edu>
2014-10-23 20:12:05 -0700

    Initial Commit

---
+++ coolbeans/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  coolbeans
+//
+//  Created by Kevin Casey on 10/23/14.
+//  Copyright (c) 2014 ieor190. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ coolbeans/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coolbeans
+//
+//  Created by Kevin Casey on 10/23/14.
+//  Copyright (c) 2014 ieor190. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
